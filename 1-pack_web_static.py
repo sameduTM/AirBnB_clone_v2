39d19c3840c42d2e0d3f4c8d3707a151fbe082f6
Ken Wekesa <wanyamak884@gmail.com>
2024-06-20 22:31:06 +0000

    fixed the pep8 error and changed the string formatting

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,13 +5,12 @@ web_static folder of your AirBnB Clone repo, using the function do_pack
 from fabric.api import local
 from datetime import datetime
 
-
 def do_pack():
-    """generates a .tgz archive from the contents of the web_static"""
+    """generates a .tgz archive from the contents of the web_static """
     x = datetime.now()
     local("mkdir -p versions")
     try:
-        local(f"tar -cvzf versions/web_static_{x.year}{x.month}\
-            {x.day}{x.hour}{x.minute}{x.second}.tgz web_static")
+        local("tar -cvzf versions/web_static_{}{}{}{}{}{}.tgz web_static".format(x.year,
+              x.month, x.day, x.hour, x.minute, x.second))
     except Exception:
         return None
