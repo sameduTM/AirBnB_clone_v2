9d158e66fc401f11970cca99f32e3efe674b1785
Ken Wekesa <wanyamak884@gmail.com>
2024-06-20 22:50:13 +0000

    added 2 blank lines after import stmts

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive from the contents of the 
+"""Fabric script that generates a .tgz archive from the contents of the
 web_static folder of your AirBnB Clone repo, using the function do_pack
 """
 from fabric.api import local
 from datetime import datetime
+
 
 def do_pack():
     """generates a .tgz archive from the contents of the web_static """
