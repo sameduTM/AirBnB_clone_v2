e151dc72fb1e22afac4982c73006047b2178dfaa
Ken Wekesa <wanyamak884@gmail.com>
2024-06-20 21:03:00 +0000

    pep8 style check passed

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,11 +5,13 @@ web_static folder of your AirBnB Clone repo, using the function do_pack
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
     """generates a .tgz archive from the contents of the web_static"""
     x = datetime.now()
     local("mkdir -p versions")
     try:
-        local(f"tar -cvzf versions/web_static_{x.year}{x.month}{x.day}{x.hour}{x.minute}{x.second}.tgz web_static")
+        local(f"tar -cvzf versions/web_static_{x.year}{x.month}\
+            {x.day}{x.hour}{x.minute}{x.second}.tgz web_static")
     except Exception:
         return None
