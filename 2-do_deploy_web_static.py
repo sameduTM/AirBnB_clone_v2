a39526d2c963b06384da2174c1d7382f672c3cdc
Ken Wekesa <wanyamak884@gmail.com>
2024-06-22 23:05:56 +0300

    remove the files and re-upload

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,14 +17,11 @@ def do_deploy(archive_path):
         try:
             put(archive_path, "/tmp/")
             run("mkdir -p /data/web_static/releases/web_static_2024620223022/")
-            run("tar -xzf /tmp/web_static_2024620223022.tgz -C\
-                /data/web_static/releases/web_static_2024620223022/")
+            run("tar -xzf /tmp/web_static_2024620223022.tgz -C /data/web_static/releases/web_static_2024620223022/")
             run("rm /tmp/web_static_2024620223022.tgz")
-            run(f"mv /data/web_static/releases/{arch_file}/web_static/*\
-                /data/web_static/releases/web_static_2024620223022/")
+            run(f"mv /data/web_static/releases/{arch_file}/web_static/* /data/web_static/releases/web_static_2024620223022/")
             run("rm -rf /data/web_static/current")
-            run("ln -s /data/web_static/releases/web_static_2024620223022/\
-                /data/web_static/current")
+            run("ln -s /data/web_static/releases/web_static_2024620223022/ /data/web_static/current")
             return True
         except Exception:
             return False
