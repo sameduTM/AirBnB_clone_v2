83cc1d653426ab7f6aca52c3f458f3e0f9eb3baa
wanyamak884@gmail.com <wanyamak884@gmail.com>
2025-02-17 13:09:14 +0300

    applocation server

---
+++ mmain.py
@@ -0,0 +1,9 @@
+"""
+ Verification
+"""
+print("")
+all_states = storage.all(State)
+for state_id, state in all_states.items():
+    for city in state.cities:
+        print("Find the city {} in the state {}".format(city, state))
+
